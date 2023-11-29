27b196eb8cdfc3cdc76328429e4393df0103344c
Andrew Butler <sephbin@gmail.com>
2023-11-29 23:50:59 +1100

    vault backup: 2023-11-29 23:50:59

--- orgObs.py
+++ orgObs.py
@@ -3,6 +3,8 @@ import yaml
 import os
 
 
+lut = {""}
+
 for root, dirs, files in os.walk(".", topdown=False):
 	#print(root, dirs, files)
 
@@ -16,38 +18,3 @@ for root, dirs, files in os.walk(".", topdown=False):
 				yamlt = yaml.safe_load(yamlt)
 				print(yamlt)
 				break
-
-
-
-import clr
-clr.AddReference('System.Core')
-clr.AddReference('RhinoInside.Revit')
-clr.AddReference('RevitAPI') 
-clr.AddReference('RevitAPIUI')
-clr.AddReference('RevitServices')
-from RevitServices.Persistence import DocumentManager
-
-from RhinoInside.Revit import Revit, Convert
-from Autodesk.Revit import DB
-import System
-
-doc = Revit.ActiveDBDocument
-app = Revit.ActiveDBApplication
-
-version = doc.GetDocumentVersion(doc)
-versionGUID = version.VersionGUID
-changedElements = doc.GetChangedElements(versionGUID)
-
-modElements = changedElements.GetCreatedElementIds()
-modElements = changedElements.GetModifiedElementIds() ## testing the different methods
-
-for mE in modElements:
-    print(mE)
-    el = doc.GetElement(mE)
-    print(el)
-    print("-"*100)
-
-
-
-
-
